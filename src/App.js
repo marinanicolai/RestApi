18359c85298cd0f7a77f338e43bd8c0a214cef70
Marina Nicolai <nicolaimarina@gmail.com>
2020-07-21 13:17:21 -0400

    Simple POST request with a JSON body using fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import List from "./components/List";
+import withListLoading from "./components/WithListLoading";
+import { PostRequest } from "./components/PostRequest";
 function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/marinanicolai/repos`;
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(repos => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>My Repositories</h1>
+      </div>
+      <div className="repo-container">
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <div>
+        <h3 className="p-3 text-center">React HTTP POST Requests with Fetch</h3>
+        <PostRequest />
+      </div>
+      <footer>
+        <div className="footer">
+          Built{" "}
+          <span role="img" aria-label="love">
+            💚
+          </span>{" "}
+          with by Marina Nicolai
+        </div>
+      </footer>
     </div>
   );
 }
-
 export default App;
